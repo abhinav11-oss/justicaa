7e7236715ef046c2a635706e65331c8aa443af5e
[dyad] <abhinavlodhi99@gmail.com>
2025-07-19 14:03:54 +0530

    [dyad] Corrected smooth scroll options - wrote 1 file(s)

--- src/hooks/useSmoothScroll.tsx
+++ src/hooks/useSmoothScroll.tsx
@@ -6,7 +6,6 @@ export const useSmoothScroll = () => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing function
-      smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
     });
